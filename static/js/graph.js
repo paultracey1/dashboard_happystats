5c80b84c280d8f1af9cb5027d2763e51c1690742
Paul Tracey <Paul Tracey>
2017-06-12 16:42:01 +0100

    all graphs done

--- static/js/graph.js
+++ static/js/graph.js
@@ -10,11 +10,69 @@ function makeGraphs(error, projectsJson) {
 
 
    happystatistics.forEach(function (d) {
-       if(d['Happiness Score'] > 5)
-           d["Sentiment"] = "Happy";
+       if(d['Happiness Score'] > 6.6)
+           d["Sentiment"] = "High Happiness";
+        else if(d['Happiness Score'] > 5.2)
+            d["Sentiment"] = "Medium Happiness";
+        else if(d['Happiness Score'] > 3.8)
+            d["Sentiment"] = "Low Happiness";
         else
-           d["Sentiment"] = "Unhappy";
+           d["Sentiment"] = "Very Low Happiness";
+   });
 
+
+   happystatistics.forEach(function (d) {
+       if(d['Family'] > 1.1)
+           d["familylvl"] = "High Family Count";
+        else if(d['Family'] > 0.9)
+            d["familylvl"] = "Medium Family Count";
+        else
+           d["familylvl"] = "Low Family Count";
+   });
+
+   happystatistics.forEach(function (d) {
+       if(d['Health (Life Expectancy)'] > .8)
+           d["healthlvl"] = "High Life Expectancy";
+        else if(d['Health (Life Expectancy)'] > .6)
+            d["healthlvl"] = "Medium Life Expectancy";
+        else
+           d["healthlvl"] = "Low Life Expectancy";
+   });
+
+   happystatistics.forEach(function (d) {
+       if(d['Economy (GDP per Capita)'] > .75)
+           d["economylvl"] = "High GDP";
+        else if(d['Economy (GDP per Capita)'] > .6)
+            d["economylvl"] = "Medium GDP";
+        else
+           d["economylvl"] = "Low GDP";
+   });
+
+   happystatistics.forEach(function (d) {
+       if(d['Freedom'] > .7)
+           d["freedomlvl"] = "High Freedom";
+        else if(d['Freedom'] > .5)
+            d["freedomlvl"] = "Medium Freedom";
+        else
+           d["freedomlvl"] = "Low Freedom";
+   });
+
+    happystatistics.forEach(function (d) {
+       if(d['Trust (Government Corruption)'] > .7)
+           d["trustlvl"] = "High Trust";
+        else if(d['Trust (Government Corruption)'] > .5)
+            d["trustlvl"] = "Medium Trust";
+        else
+           d["trustlvl"] = "Low Trust";
+   });
+
+   happystatistics.forEach(function (d) {
+       if(d['Generosity'] > .7)
+           d["generositylvl"] = "High Generosity";
+        else if(d['Generosity'] > .5)
+            d["generositylvl"] = "Medium Generosity";
+        else
+           d["generositylvl"] = "Low Generosity";
    });
  
  
@@ -35,6 +93,24 @@ function makeGraphs(error, projectsJson) {
    var sentimentDim = ndx.dimension(function (d) {
        return d["Sentiment"];
    });
+   var familylvlDim = ndx.dimension(function (d) {
+       return d["familylvl"];
+   });
+    var healthlvlDim = ndx.dimension(function (d) {
+       return d["healthlvl"];
+   });
+   var economylvlDim = ndx.dimension(function (d) {
+       return d["economylvl"];
+   });
+   var freedomlvlDim = ndx.dimension(function (d) {
+       return d["freedomlvl"];
+   });
+   var trustlvlDim = ndx.dimension(function (d) {
+       return d["trustlvl"];
+   });
+   var generositylvlDim = ndx.dimension(function (d) {
+       return d["generositylvl"];
+   });
 
 
  
@@ -44,6 +120,13 @@ function makeGraphs(error, projectsJson) {
    var numProjectsByRegion = regionDim.group();
    var numProjectsByFamily = familyDim.group();
    var sentimentCount = sentimentDim.group();
+
+   var familylvlCount = familylvlDim.group();
+   var healthlvlCount = healthlvlDim.group();
+   var economylvlCount = economylvlDim.group();
+   var freedomlvlCount = freedomlvlDim.group();
+   var trustlvlCount = trustlvlDim.group();
+   var generositylvlCount = generositylvlDim.group();
 
    var totalhappinessbyregion = regionDim.group().reduceSum(function(d){
        return d["Happiness Score"]
@@ -195,7 +278,16 @@ function makeGraphs(error, projectsJson) {
     var regionHappinessChart = dc.barChart("#region-happiness-chart")
 //    var numberProjectsND = dc.numberDisplay("#number-projects-nd");
 //    var totalDonationsND = dc.numberDisplay("#total-donations-nd");
-   var fundingStatusChart = dc.pieChart("#funding-chart");
+   var familypiechart = dc.pieChart("#family-chart");
+   var healthpiechart = dc.pieChart("#health-chart");
+   var happyCounts = dc.pieChart("#happy-counts");
+   var economypiechart = dc.pieChart("#economy-chart");
+   var freedompiechart = dc.pieChart("#freedom-chart");
+   var trustpiechart = dc.pieChart("#trust-chart");
+   var generositypiechart = dc.pieChart("#generosity-chart");
+
+
+   
 
  
  
@@ -241,11 +333,7 @@ function makeGraphs(error, projectsJson) {
        .height(200)
        .margins({top: 10, right: 50, bottom: 30, left: 50})
        .dimension(regionDim)
-       .group(averagehappinessbyregion)
-       .valueAccessor(function(p){
-           return p.value.average;
-       })
-
+       .group(sentimentCount)
        .transitionDuration(500)
        .x(d3.scale.ordinal())
        .xUnits(dc.units.ordinal)
@@ -267,13 +355,74 @@ function makeGraphs(error, projectsJson) {
        .group(numProjectsByRegion)
        .xAxis().ticks(4);
  
-   fundingStatusChart
+   familypiechart
        .height(220)
        .radius(90)
        .innerRadius(40)
        .transitionDuration(1500)
-       .dimension(regionDim)
-       .group(numProjectsByRegion);
+       .dimension(familylvlDim)
+       .group(familylvlCount);
+
+    happyCounts
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(sentimentDim)
+       .group(sentimentCount);
+
+    healthpiechart
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(healthlvlDim)
+       .group(healthlvlCount);
+
+    economypiechart
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(economylvlDim)
+       .group(economylvlCount);
+
+    freedompiechart
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(freedomlvlDim)
+       .group(freedomlvlCount);
+
+    trustpiechart
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(trustlvlDim)
+       .group(trustlvlCount);
+
+    generositypiechart
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(generositylvlDim)
+       .group(generositylvlCount);
+
+
+    //    happyCounts
+    //    .height(220)
+    //    .radius(90)
+    //    .innerRadius(40)
+    //    .transitionDuration(1500)
+    //    .dimension(regionDim)
+    //    .group(averagehappinessbyregion)
+    //    .valueAccessor(function(p){
+    //        return p.value.average;
+    //    })
+
 
  
  
